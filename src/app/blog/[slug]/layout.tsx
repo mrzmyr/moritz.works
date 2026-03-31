36d8b775c11927f83015a511247ea4f68aa8482c
mrzmyr <moritz@devmood.com>
2026-03-31 09:49:39 +0200

    refactor: simplify layout structure in blog post component
    - Removed unnecessary wrapper elements in the layout.tsx file to streamline the rendering of post metadata and headline.
    - Enhanced readability and maintainability of the layout by directly displaying the PostHeadline and PostMetadata components.
    - Improved overall structure for better performance and clarity in the blog post layout.

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -4,10 +4,8 @@ import { PostContentConatiner } from "@/components/post-content-container";
 import { PostHeadline } from "@/components/post-headline";
 import { PostMetadata } from "@/components/post-metadata";
 import { PostStructuredData } from "@/components/post-structured-data";
-import { PostCopyMarkdown } from "@/components/ui/post-copy-markdown";
 import { siteConfig } from "@/config/app";
 import { getPostMetadata } from "@/lib/posts";
-import { getAbsolutePostUrl } from "@/lib/urls";
 
 export default async function Layout({
   children,
@@ -36,21 +34,11 @@ export default async function Layout({
         image={`${siteConfig.url}/static/og/default.png`}
       />
       <div className="my-12">
-        <div className="flex justify-between items-start">
-          <div>
-            <PostHeadline>{post.title}</PostHeadline>
-            <PostMetadata
-              createdAt={new Date(post.createdAt)}
-              updatedAt={new Date(post.updatedAt)}
-            />
-          </div>
-          {post.content && (
-            <PostCopyMarkdown
-              content={post.content}
-              url={getAbsolutePostUrl({ slug: post.slug })}
-            />
-          )}
-        </div>
+        <PostHeadline>{post.title}</PostHeadline>
+        <PostMetadata
+          createdAt={new Date(post.createdAt)}
+          updatedAt={new Date(post.updatedAt)}
+        />
       </div>
       <PostContentConatiner>{children}</PostContentConatiner>
     </>
